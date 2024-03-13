095cc6855d897da400b7455a56f11e2a47d097cc
Noorie Esmaili <noorie.esmaili@gmail.com>
2024-03-13 23:09:04 +0100

    update readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Södra Teknisk Uppgift
+
+### 1. SQL
+
+```
+SELECT Avdelning, COUNT(*) FROM Medarbetare GROUP BY Avdelning;
+```
+### 2. Imperativ programmering
+
+I den här lösningen har jag använt HashSet för att lagra tecken i en unik uppsättning. Jag har sett till att endast använda små bokstäver för att göra det snabbare att kontrollera strängen.
+ 
--- src/main/java/se/sodra/Main.java
+++ src/main/java/se/sodra/Main.java
@@ -4,7 +4,14 @@ import java.util.HashSet;
 
 public class Main {
 
-    public static boolean hasUniqueChars(String text) {
+    /**
+     * Kontrollerar om en textsträng endast innehåller unika tecken.
+     * Ignorerar mellanslag och tar hänsyn till enbart små bokstäver.
+     *
+     * @param text Textsträngen att kontrollera.
+     * @return True om texten har endast unika tecken, annars false.
+     */
+    private static boolean hasUniqueChars(String text) {
         HashSet<Character> set = new HashSet<>();
 
         for (char c : text.toCharArray()) {
@@ -20,6 +27,13 @@ public class Main {
         return true;
     }
 
+
+    /**
+     * Visar resultatet av kontrollen för unika tecken i en textsträng.
+     *
+     * @param text Textsträngen.
+     * @param isUnique True om texten har endast unika tecken, annars false.
+     */
     private static void displayResult(String text, boolean isUnique) {
         String message = isUnique ? "har endast unika tecken." : "har duplicerade tecken.";
         System.out.println("Texten \"" + text + "\" " + message);
